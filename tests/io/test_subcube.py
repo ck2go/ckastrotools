1519a266cdab8fdc1c223fa92d5ef247f4f407f0
Carsten König <koenig@mpifr-bonn.mpg.de>
2022-12-19 11:46:23 +0100

    Added method to extract subcubes in galactic coordinates.

---
+++ tests/io/test_subcube.py
@@ -0,0 +1,17 @@
+"""Test the subcube module."""
+
+from unittest.mock import Mock
+
+from spectral_cube import SpectralCube
+
+from ckastro.io.subcube import extractCutout
+
+
+def test_extractCutout(mocker):
+    mock_cube = Mock()
+    mock_cube.subcube.return_value = Mock(spec=SpectralCube)
+
+    result = extractCutout(mock_cube, glon=42.0, glat=0., width=0.5)
+
+    assert mock_cube.subcube.called_once()
+    assert isinstance(result, SpectralCube)
